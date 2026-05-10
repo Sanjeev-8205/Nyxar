a2a8ef7b54b427c2611d3cd0abab1e9fb206099c
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-05-10 19:59:35 +0530

    Remove unused import of InputData from dashboard.py

--- backend/app/routes/dashboard.py
+++ backend/app/routes/dashboard.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from app.services.metrics_service.dashboard_metrics_service import dashboard_metrics_aggregator
-from app.schemas.request_schema import InputData
 
 router = APIRouter()
 
