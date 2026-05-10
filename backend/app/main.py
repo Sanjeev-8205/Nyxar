9d7b7a18c8a4ba9883e386c5230bbfb5afc47396
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-05-10 15:00:04 +0530

    Remove logs router from main application setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.routes.models import router as models_router
-from app.routes.logs import router as logs_router
 from app.routes.analytics import router as analytics_router
 from app.routes.count_predictions import router as counts_router
 from app.routes.predict import router as prediction_router
@@ -32,7 +31,6 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(models_router)
 app.include_router(prediction_router)
-app.include_router(logs_router)
 app.include_router(analytics_router)
 app.include_router(counts_router)
 app.include_router(latency_router)
