091a4e64c085b342aced090c3f6d3b51f158003a
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-06-25 23:27:20 +0530

    fix: modify logging_config.py

--- backend/app/core/logging_config.py
+++ backend/app/core/logging_config.py
@@ -1,2 +1,20 @@
 import structlog
 import logging
+
+def setup_logging():
+
+    logging.basicConfig(
+        format="%(message)s",
+        level=logging.INFO
+    )
+
+    structlog.configure(
+        processors=[
+            structlog.stdlib.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.format_exc_info,
+            structlog.processors.JSONRenderer()
+        ]
+    )
+
+    wrapper_class=structlog.stdlib.BoundLogger
